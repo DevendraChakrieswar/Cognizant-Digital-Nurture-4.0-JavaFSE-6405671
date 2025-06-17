fc1b13e0442ea84219700d043947731ee0c91fc5
Chitroju Devendra Chakrieswar <devendrachitroju555@gmail.com>
2025-06-17 22:33:48 +0530

    Done excersise 1 Singleton pattern

---
+++ Week1/EngineeringConcepts/DesignPatternsAndPrinciples/SinglePatternExample/SingletonTest.java
@@ -0,0 +1,14 @@
+package Week1.EngineeringConcepts.DesignPatternsAndPrinciples.SinglePatternExample;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+
+        Logger obj1 = Logger.getInstance();
+        obj1.log("First log message");
+
+        Logger obj2 = Logger.getInstance();
+        obj2.log("Second log message");
+
+        System.out.println("Same instance? " + (obj1 == obj2));
+    }
+}
